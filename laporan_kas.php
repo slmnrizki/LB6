72cc7e322673b1bb7b1e96af445517faa9c7c348
slmnrizki <117463412+slmnrizki@users.noreply.github.com>
2023-06-28 05:03:14 +0700

    Add files via upload

---
+++ laporan_kas.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <link href="style.css" rel="stylesheet">
+    <!-- custom CSS -->
+    <title>Laporan kas</title>
+  </head>
+  <body>
+    <div class="global-container">
+        <div class="card login-form">
+            <div class="card-body">
+                <h5 class="card title text-center">Laporan KAS</h5>
+            </div>
+            <div class="card-text">
+                <form>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Tanggal</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Pengeluaran</label>
+    <input type="password" class="form-control" id="exampleInputPassword1">
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Jumlah</label>
+    <input type="password" class="form-control" id="exampleInputPassword1">
+  </div>
+  <div class="d-grid gap-2">
+    <button type="submit" class="btn btn-primary">Tambah</button>
+  </div>
+</form>
+            </div>
+        </div>
+    </div>
+    
+  </body>
+</html>
